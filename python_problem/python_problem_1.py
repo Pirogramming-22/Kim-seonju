adad15331fe55ff5fef19409dc994b78e7863e35
seonjuuu <mhnaa6469@gmail.com>
2025-01-02 22:39:25 +0900

    3단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,3 +1,15 @@
 num = 0
 
-cnt = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+while(1) :
+    cnt = (input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+    if not cnt.isdigit():
+        print("정수를 입력하세요")
+        continue
+    
+    cnt = int(cnt)  #정수 변환
+    
+    if cnt not in [1,2,3] :
+        print("1,2,3 중 하나를 입력하세요")
+        
+    else:
+        break
