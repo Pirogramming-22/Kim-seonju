aa95eb352b641602eac5a2a691bf4eec23c83101
seonjuuu <mhnaa6469@gmail.com>
2025-01-03 00:09:47 +0900

    6단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -24,7 +24,7 @@ while(1) :
 for i in range(cnt):
     num += 1
     print(f"playerA : {num}")
-'''
+
 
 #5단계
 players = ['player1', 'player2']
@@ -51,3 +51,34 @@ while(num < 31) :
         print(f"{player} : {num}")
     
     turn += 1
+'''
+
+#6단계
+players = ['player1', 'player2']
+turn = 0
+while(num < 31) :
+    player = players[turn%2] #번갈아가면서
+    
+    while(1):
+        cnt = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+        if not cnt.isdigit():
+            print("정수를 입력하세요")
+            continue
+            
+        cnt = int(cnt)
+        
+        if cnt not in [1,2,3] :
+            print("1,2,3 중 하나를 입력하세요")
+            
+        else:
+            break
+
+    for i in range(cnt):
+        num += 1
+        print(f"{player} : {num}")
+        if num == 31 :
+            break
+    
+    turn += 1
+
+
