10772dd668675131be094c29963b4a3a31041063
seonjuuu <mhnaa6469@gmail.com>
2025-01-07 21:59:12 +0900

    Django 뷰 만들기

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {}) #render 메서드를 호출하여 blog/post_list.html템플릿을 보여줌
